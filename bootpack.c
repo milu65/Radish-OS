06c042930e10feab53e2f23a6cf7b23c2d65314b
milu65 <millby@yahoo.com>
2020-08-08 02:39:53 -0300

    display memory change

--- bootpack.c
+++ bootpack.c
@@ -1,8 +1,17 @@
 
 void io_hlt(void);
+void write_mem8(int addr,int data);
 
 void HariMain(void){
-	fin:
-		io_hlt();
-		goto fin;
+	int i;
+	char*p;
+	for(i=0xa0000;i<0xaffff;i++){
+		// p=(char *)i;
+		// *p=i&15;
+		*((char *)i)=(i&15);
+		//write_mem8(i,i^15);
+	}
+		
+	
+	while(1){io_hlt();}
 }
--- naskfunc.nas
+++ naskfunc.nas
@@ -1,12 +1,13 @@
 ; naskfunc
 
 [FORMAT "WCOFF"]	;制作目标文件的模式
+[INSTRSET "i486p"]				;使用到486为止的指令
 [BITS 32]	;制作32位模式用的机械语言
 
 ;制作目标文件的信息
 
 [FILE "naskfunc.nas"]	;源文件名信息
-	GLOBAL _io_hlt
+	GLOBAL _io_hlt,_write_mem8
 
 ;以下是实际函数
 
