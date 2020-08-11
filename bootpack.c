11bb59a05108a50108c32345202e229724e8d8cf
milu65 <millby@yahoo.com>
2020-08-10 21:51:41 -0300

    draw something

--- bootpack.c
+++ bootpack.c
@@ -1,17 +1,104 @@
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
 
 void io_hlt(void);
-void write_mem8(int addr,int data);
+void io_cli(void);
+void io_sti(void);
+void io_stihlt(void);
+void io_out8(int port, int data);
+void io_out16(int port, int data);
+void io_out32(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+void set_palette(int start,int end,unsigned char*rgb){
+	int i;
+	int flag=io_load_eflags();//记录中断许可的标识的值
+	io_cli();	//将中断许可标识设置为0,禁止中断
+	io_out8(0x03c8,start);
+	for(i=start;i<=end;i++){
+		io_out8(0x03c9,rgb[0]/4);
+		io_out8(0x03c9,rgb[1]/4);
+		io_out8(0x03c9,rgb[2]/4);
+		rgb += 3;
+	}
+	io_store_eflags(flag);
+	return;
+}
+
+void init_palette(void)
+{
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00,	/*  0:黑 */
+		0xff, 0x00, 0x00,	/*  1:亮红 */
+		0x00, 0xff, 0x00,	/*  2:亮绿 */
+		0xff, 0xff, 0x00,	/*  3:亮黄 */
+		0x00, 0x00, 0xff,	/*  4:亮蓝 */
+		0xff, 0x00, 0xff,	/*  5:亮紫 */
+		0x00, 0xff, 0xff,	/*  6:浅亮蓝 */
+		0xff, 0xff, 0xff,	/*  7:白 */
+		0xc6, 0xc6, 0xc6,	/*  8:亮灰 */
+		0x84, 0x00, 0x00,	/*  9:暗红 */
+		0x00, 0x84, 0x00,	/* 10:暗绿 */
+		0x84, 0x84, 0x00,	/* 11:暗黄 */
+		0x00, 0x00, 0x84,	/* 12:暗青 */
+		0x84, 0x00, 0x84,	/* 13:暗紫 */
+		0x00, 0x84, 0x84,	/* 14:浅暗蓝 */
+		0x84, 0x84, 0x84	/* 15:暗灰 */
+	};
+	set_palette(0, 15, table_rgb);
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+{
+	int x, y;
+	for (y = y0; y <= y1; y++) {
+		for (x = x0; x <= x1; x++)
+			vram[y * xsize + x] = c;
+	}
+	return;
+}
 
 void HariMain(void){
-	int i;
-	char*p;
-	for(i=0xa0000;i<0xaffff;i++){
-		// p=(char *)i;
-		// *p=i&15;
-		*((char *)i)=(i&15);
-		//write_mem8(i,i^15);
-	}
-		
+	init_palette();
+	char*vram=0xa0000;
+	int xsize = 320;
+	int ysize = 200;
+
+	boxfill8(vram, xsize, COL8_008484,  0,         0,          xsize -  1, ysize - 29);
+	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 28, xsize -  1, ysize - 28);
+	boxfill8(vram, xsize, COL8_FFFFFF,  0,         ysize - 27, xsize -  1, ysize - 27);
+	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 26, xsize -  1, ysize -  1);
+
+	boxfill8(vram, xsize, COL8_FFFFFF,  3,         ysize - 24, 59,         ysize - 24);
+	boxfill8(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
+	boxfill8(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
+	boxfill8(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
+
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+	
+	boxfill8(vram,320,COL8_FF0000,20,20,120,120);
+	boxfill8(vram,320,COL8_00FF00,70,50,170,150);
+	boxfill8(vram,320,COL8_0000FF,120,80,220,180);
 	
 	while(1){io_hlt();}
 }
--- naskfunc.nas
+++ naskfunc.nas
@@ -7,7 +7,11 @@
 ;制作目标文件的信息
 
 [FILE "naskfunc.nas"]	;源文件名信息
-	GLOBAL _io_hlt,_write_mem8
+
+		GLOBAL	_io_hlt, _io_cli, _io_sti, _io_stihlt
+		GLOBAL	_io_in8,  _io_in16,  _io_in32
+		GLOBAL	_io_out8, _io_out16, _io_out32
+		GLOBAL	_io_load_eflags, _io_store_eflags
 
 ;以下是实际函数
 
@@ -22,4 +26,44 @@ _write_mem8:	;void write_mem8(int addr,int data);
 	MOV [ECX],AL
 	RET
 	
+_io_cli: ;void io_cli(void);
+	CLI
+	RET
 	
+_io_sti: ;void io_sti(void);
+	STI
+	RET
+	
+_io_stihlt: ;void io_stihlt(void);
+	STI
+	CLI
+	RET
+
+_io_out8:	; void io_out8(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		AL,[ESP+8]		; data
+		OUT		DX,AL
+		RET
+
+_io_out16:	; void io_out16(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,AX
+		RET
+
+_io_out32:	; void io_out32(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,EAX
+		RET
+
+_io_load_eflags:	; int io_load_eflags(void);
+		PUSHFD		; PUSH EFLAGS
+		POP		EAX
+		RET
+
+_io_store_eflags:	; void io_store_eflags(int eflags);
+		MOV		EAX,[ESP+4]
+		PUSH	EAX
+		POPFD		; POP EFLAGS
+		RET
